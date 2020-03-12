720d805c1ca9a93153c51f6dc07059687f1e28e6
emman-github <emmanuel.hernandez.freelancer@gmail.com>
2020-03-12 10:03:23 +0800

    Setup httclient, added api service and tried to connect the login page to local and online API

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api/api.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(
+  	private apiService: ApiService
+  ) {
+
+  }
 
   ngOnInit() {
+  	this.login();
+  }
+
+  login() {
+  	this.apiService.login().then(data => {
+  	  console.log(data);
+  	});
   }
 
 }
--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  baseUrl: string;
 
-  constructor() { }
+  constructor(
+  	private httpClient: HttpClient
+  ) {
+  	//this.baseUrl = 'http://localhost/KBB/index.php/Welcome/';
+  	this.baseUrl = 'http://kbb-back-end.000webhostapp.com/index.php/Welcome/';
+  }
+
+  login(): Promise<any> {
+
+    const promise = new Promise((resolve, reject) => { 
+      let url: string = this.baseUrl + 'login';  
+
+      let params = new FormData(); 
+ 
+      this.httpClient.get(url).subscribe(response => {
+        resolve(response);
+      }, error => {
+        alert(JSON.stringify(error));
+      });
+    });
+
+    return promise;
+  }  
 }
